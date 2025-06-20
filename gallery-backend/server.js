30f9e17c721bc80d6557627e635b066f354e42ee
Muhammad Arkan Nadhif Rafsanjani <125430945+Shibarkan@users.noreply.github.com>
2025-06-20 16:08:39 +0700

    dotenv

--- gallery-backend/server.js
+++ gallery-backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config();
+
 
 const app = express();
 const PORT = 3000;
